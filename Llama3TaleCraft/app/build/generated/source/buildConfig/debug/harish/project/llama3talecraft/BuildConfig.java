e7818a73ffceac6c3272bae2d68a7b2a723b5646
Satan Harry <harishmahto2003@gmail.com>
2024-07-21 16:21:45 +0530

    Add files via upload

---
+++ Llama3TaleCraft/app/build/generated/source/buildConfig/debug/harish/project/llama3talecraft/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package harish.project.llama3talecraft;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "harish.project.llama3talecraft";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String apiKey = "AIzaSyC3C7qJP3jrTz4SEUG4zbNlL6EgLNwYdg4";
+}
