fbe4a20520c02eced86d5b51045c266a59d4a62d
Satan Harry <harishmahto2003@gmail.com>
2024-07-21 16:09:44 +0530

    Add files via upload

---
+++ Llama3TaleCraft/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Llama3 Tale Craft"
+include(":app")
+ 
